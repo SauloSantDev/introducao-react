e11a9803842773d45e97910633ea7bf4b6994620
Saulo Santana <saulocostasan@gmail.com>
2021-12-08 10:43:02 -0300

    adicionando categorias dinâmicas

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
 import React, { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro";
 import ListadeNotas from "./components/ListadeNotas";
+import ListaCategorias from "./components/ListaCategorias";
 import "./assets/estilos/App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      notas: []
-    }
+      notas: [],
+      categorias: []
+    };
     // this.notas =[];
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    // this.notas.push(novaNota);
-    // console.log(this.notas.length)
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
     const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas: novoArrayNotas
-    }
+      notas: novoArrayNotas,
+    };
     this.setState(novoEstado);
   }
 
-  deletarNota(index){
+  deletarNota(index) {
     let arrayNotas = this.state.notas;
     arrayNotas.splice(index, 1);
-    this.setState({notas:arrayNotas})
+    this.setState({ notas: arrayNotas });
   }
 
-  render(){
+  adicionarCategoria(nomeCategoria){
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+    const novoEstado = {
+      categorias: novoArrayCategorias,
+    };
+    this.setState(novoEstado);
+  }
+
+  render() {
     return (
       <section className="conteudo">
-      <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-      <ListadeNotas notas={this.state.notas} deletarNota={this.deletarNota.bind(this)}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <main className="conteudo-principal">
+          <ListaCategorias categorias={this.state.categorias} adicionarCategoria={this.adicionarCategoria.bind(this)}/>
+          <ListadeNotas
+            notas={this.state.notas}
+            deletarNota={this.deletarNota.bind(this)}
+          />
+        </main>
       </section>
     );
   }
